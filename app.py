97c60f79ffc950bd7a5a67f67d7f8a5dc8b80e80
Bruno Otto Cavalcante Vieira <brunocavieira@outlook.com>
2026-05-06 14:01:12 -0300

    atualização cod

--- app.py
+++ app.py
@@ -10,5 +10,17 @@ def index():
 def saudacao():
     return render_template("saudacao.html")
 
+@app.route('/cursos')
+def curso():
+    return render_template("cursos.html")
+
+@app.route('/curso/devweb')
+def curso_devweb():
+    return render_template("curso_devweb.html")
+
+@app.route('/curso/poo')
+def curso_poo():
+    return render_template("curso_poo.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
